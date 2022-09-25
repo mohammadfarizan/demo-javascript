e4ec07d90abd7abde10695ff335aca0fa2b48c25
mohammad farizan <mohammadfarizan224@gmail.com>
2022-09-25 10:17:44 +0700

    feat: add pop up box and if else

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// coba console.log, alert, prompt dan confirm 
+// confirm itu buat yang ada ok dan cancel nya
+
 console.log("Hello World");
+
+alert("Hi There!");
+
+const userName = prompt("Masukkan nama kamu: ");
+console.log("Hi " + userName)
+
+let answer = confirm("Silahkan pilih OK atau cancel");
+let result = answer ? "Kamu memilih ok" : "kamu memilih cancel";
+alert(result);
+
+//if else statement
+
+const number = prompt("silahkan masukkan angka: ");
+
+if (number > 0){
+    console.log("angka termasuk bilangan positif");
+}else if (number < 0){
+    console.log("angka termasuk bilangan negatif");
+}else if (number == 0){
+    console.log("angka yang dimasukkan adalah 0");
+}else{
+    console.log("yang anda masukkan bukanlah angka");
+}
